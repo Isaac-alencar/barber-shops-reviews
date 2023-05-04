6f86536e981af37948378805db4142bed2cb129b
Isaac-alencar <alencarisaac3@gmail.com>
2023-05-04 17:49:19 -0300

    change json-server port from 3000 to 3001

--- src/infra/apiClient.ts
+++ src/infra/apiClient.ts
@@ -2,6 +2,6 @@ import axios, { AxiosRequestConfig } from "axios";
 
 export const apiClient = (config?: AxiosRequestConfig) =>
   axios.create({
-    baseURL: " http://localhost:3000/",
+    baseURL: " http://localhost:3001/",
     ...config,
   });
