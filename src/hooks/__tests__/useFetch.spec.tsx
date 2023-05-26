0e65853e17316f5789759f833c9b917881d5a6f4
Bruno Viveiros <bruno.viveiros@codeminer42.com>
2023-05-26 11:40:40 -0300

    tests: refactor useFetch tests

--- src/hooks/__tests__/useFetch.spec.tsx
+++ src/hooks/__tests__/useFetch.spec.tsx
@@ -1,10 +1,25 @@
-import { act, renderHook, waitFor } from "@testing-library/react";
-import { useFetch } from "../useFetch";
+import { act, renderHook, waitFor } from '@testing-library/react';
+import { useFetch } from '../useFetch';
 
-describe("useFetch", () => {
-  describe("When successfully fetches the API", () => {
-    it("should return the expected data", async () => {
-      const queryFn = jest.fn().mockReturnValue(Promise.resolve(["testing"]));
+describe('useFetch', () => {
+  it('calls the core function one time', async () => {
+    const queryFn = jest.fn().mockResolvedValue('');
+
+    renderHook(() =>
+      useFetch({
+        queryFunction: queryFn,
+        initialData: [],
+      })
+    );
+
+    await waitFor(() => {
+      expect(queryFn).toBeCalledTimes(1);
+    });
+  });
+
+  describe('When successfully fetches the API', () => {
+    it('should return the expected data', async () => {
+      const queryFn = jest.fn().mockResolvedValue(['testing']);
 
       const { result } = renderHook(() =>
         useFetch({
@@ -14,32 +29,37 @@ describe("useFetch", () => {
       );
 
       expect(result.current.data).toEqual([]);
+      expect(result.current.isLoading).toEqual(true);
+      expect(result.current.error).toEqual('');
+
       await waitFor(() => {
-        expect(queryFn).toBeCalledTimes(1);
+        expect(result.current.data).toEqual(['testing']);
+        expect(result.current.isLoading).toEqual(false);
+        expect(result.current.error).toEqual('');
       });
-      expect(result.current.data).toEqual(["testing"]);
     });
   });
 
-  describe("When the API request fails", () => {
-    it("should return an error message", async () => {
-      const queryFn = jest
-        .fn()
-        .mockReturnValue(Promise.reject({ message: "Error" }));
-
+  describe('When the API request fails', () => {
+    it('should return an error message', async () => {
+      const queryFn = jest.fn().mockRejectedValue(new Error('Error'));
+      const initialData: never[] = [];
       const { result } = renderHook(() =>
         useFetch({
           queryFunction: queryFn,
-          initialData: [],
+          initialData,
         })
       );
 
-      expect(result.current.error).toEqual("");
+      expect(result.current.error).toEqual('');
+      expect(result.current.isLoading).toEqual(true);
+      expect(result.current.data).toEqual(initialData);
+
       await waitFor(() => {
-        expect(queryFn).toBeCalledTimes(1);
+        expect(result.current.error).toEqual('Error');
         expect(result.current.isLoading).toBe(false);
+        expect(result.current.data).toEqual(initialData);
       });
-      expect(result.current.error).toEqual("Error");
     });
   });
 });
