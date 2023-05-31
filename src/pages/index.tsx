b6baf5ae76d2bb2ad4e7ea634d5969d4a51dfe6a
Isaac-alencar <alencarisaac3@gmail.com>
2023-05-31 17:14:25 -0300

    wip

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,29 +1,46 @@
-import { useFetch } from "@/hooks/useFetch";
-import { fetchBarbersShop } from "@/infra/fetchBarbersShop";
+import { GetServerSideProps } from "next";
+import { prisma } from "@/lib/prisma";
 
-import Error from "@/components/Error";
-import Loading from "@/components/Loading";
+// import { useFetch } from "@/hooks/useFetch";
+// import { fetchBarbersShop } from "@/infra/fetchBarbersShop";
+
+// import Error from "@/components/Error";
+// import Loading from "@/components/Loading";
 
 import { BarberShopList } from "@/components/BarberShopList";
 
 import styles from "../styles/pages/Home.module.css";
 
-export default function Home() {
-  const {
-    data: barberShops,
-    error,
-    isLoading,
-  } = useFetch({
-    queryFunction: fetchBarbersShop,
-    initialData: [],
-  });
+export default function Home(props: any) {
+  console.log(props);
+  // const {
+  //   data: barberShops,
+  //   error,
+  //   isLoading,
+  // } = useFetch({
+  //   queryFunction: fetchBarbersShop,
+  //   initialData: [],
+  // });
 
-  if (error) return <Error />;
+  // if (error) return <Error />;
 
   return (
     <main className={styles.container}>
       <h1>BarbersShop Review</h1>
-      {isLoading ? <Loading /> : <BarberShopList barberShops={barberShops} />}
+      <BarberShopList barberShops={props.data} />
+      {/* {isLoading ? <Loading /> : 
+      } */}
     </main>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const data = await prisma.barber_shops.findMany();
+  console.log(data);
+
+  return {
+    props: {
+      data,
+    },
+  };
+};
